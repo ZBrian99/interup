04d9403addead7062b36de739d3489f35fc98053
ZBrian99 <brianzelada1999@gmail.com>
2024-12-01 23:11:55 -0300

    refactor: update styles for hero, header and wpp button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -196,6 +196,7 @@ export default {
 				sm: '0 2px 4px rgba(0, 0, 0, 0.05)', // Sutil, para elementos hover
 				md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)', // Cards, elementos elevados
 				lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)', // Modales, elementos destacados
+
 			},
 		},
 	},
